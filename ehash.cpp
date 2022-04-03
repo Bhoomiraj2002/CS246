16d0c077a4a945fb174ca106b5c62439b50b65ca
Bhoomiraj2002 <bhoomiraj0205@gmail.com>
2022-04-03 23:08:05 +0530

    lazy delete

--- ehash.cpp
+++ ehash.cpp
@@ -139,16 +139,6 @@ class ehash{
             int loc = x%(1<<global_depth);
             if(dirs[loc]->exists(x)){
                 dirs[loc]->del(x);
-                //check if bucket is empty
-                if(dirs[loc]->getpos()==0){
-                    //delete pointer to bucket and from list
-                    int x=0;
-                    while(x<loc)x<<1;
-                    b.remove(*dirs[loc]);
-                    dirs[loc]=dirs[loc-x/2];
-                    //check if dir is duplicate or not in loop 
-                }
-               
             }
 
         }
